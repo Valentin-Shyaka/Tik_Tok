936131293c1cfcc8a692ea5ef3f50ead0ad9e8d4
Valentin-Shyaka <vava1lantern7@gmail.com>
2023-01-07 22:12:02 +0200

    new video card component

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -6,6 +6,11 @@ import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
 import { BsFillPlayFill, BsFillPauseFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
 import { BsPlay } from 'react-icons/bs';
+import { FaBell, FaHeart } from 'react-icons/fa';
+import { FaComment } from 'react-icons/fa';
+import { FaUpload } from 'react-icons/fa';
+import { MdFavorite } from 'react-icons/md';
+
 import LikeButton from './LikeButton';
 
 import { Video } from './../types';
@@ -63,6 +68,9 @@ const VideoCard: NextPage<IProps> = ({ post: { caption, postedBy, video, _id, li
   }
 
   return (
+    <>
+    
+    <div className='flex flex-row'>
     <div className='flex flex-col border-b-2 border-gray-200 pb-6'>
       <div>
         <div className='flex gap-3 p-2 cursor-pointer font-semibold rounded '>
@@ -97,9 +105,11 @@ const VideoCard: NextPage<IProps> = ({ post: { caption, postedBy, video, _id, li
             </Link>
           </div>
         </div>
+       
+
       </div>
 
-      <div className='lg:ml-20 flex gap-4 relative'>
+      <div className='w-80 flex gap-4 relative'>
         <div
           onMouseEnter={() => setIsHover(true)}
           onMouseLeave={() => setIsHover(false)}
@@ -115,7 +125,7 @@ const VideoCard: NextPage<IProps> = ({ post: { caption, postedBy, video, _id, li
           </Link>
 
           {isHover && (
-            <div className='absolute bottom-6 cursor-pointer left-8 md:left-14 lg:left-0 flex gap-10 lg:justify-between w-[100px] md:w-[50px] lg:w-[600px] p-3'>
+            <div className='absolute bottom-6 cursor-pointer left-8 md:left-2 lg:left-0 flex gap-6 lg:justify-between w-[100px] md:w-[100px] lg:w-[350px] p-3'>
               {playing ? (
                 <button onClick={onVideoPress}>
                   <BsFillPauseFill className='text-black text-2xl lg:text-4xl' />
@@ -140,7 +150,256 @@ const VideoCard: NextPage<IProps> = ({ post: { caption, postedBy, video, _id, li
           )}
         </div>
       </div>
+      
     </div>
+    <div className="w-[200px]  h-[200px] pt-40 pl-20">
+      <div className='flex font-sans text-lg text-center pt-4'>
+      <div className='bg-primary rounded-full p-2 md:p-4 text-[#F51997] '>
+          <MdFavorite className='text-lg md:text-2xl' />
+        </div>
+            
+      </div>
+      <div className='flex font-sans text-lg text-center  pt-4'>
+      <div className='bg-primary rounded-full p-2 md:p-4 text-[#F51997] '>
+          <FaComment className='text-lg md:text-2xl' />
+        </div>
+           
+      </div>
+      <div className='flex font-sans text-lg text-center pt-4'>
+      <div className='bg-primary rounded-full p-2 md:p-4 text-[#F51997] '>
+          <FaBell className='text-lg md:text-2xl' />
+        </div>
+        </div>
+          
+      <div className='flex font-sans text-lg text-center pt-4'>
+      <div className='bg-primary rounded-full p-2 md:p-4 text-[#F51997] '>
+          <FaUpload className='text-lg md:text-2xl' />
+        </div>
+           
+      </div>
+      </div>
+    </div>
+    <div className='flex flex-row'>
+    <div className='flex flex-col border-b-2 border-gray-200 pb-6'>
+      <div>
+        <div className='flex gap-3 p-2 cursor-pointer font-semibold rounded '>
+          <div className='md:w-16 md:h-16 w-10 h-10'>
+            <Link href={`/profile/${postedBy?._id}`}>
+              <>
+                <Image
+                  width={62}
+                  height={62}
+                  className=' rounded-full'
+                  src={postedBy?.image}
+                  alt='user-profile'
+                  layout='responsive'
+                />
+              </>
+            </Link>
+          </div>
+          <div>
+            <Link href={`/profile/${postedBy?._id}`}>
+              <div className='flex items-center gap-2'>
+                <p className='flex gap-2 items-center md:text-md font-bold text-primary'>
+                  {postedBy.userName}{' '}
+                  <GoVerified className='text-blue-400 text-md' />
+                </p>
+                <p className='capitalize font-medium text-xs text-gray-500 hidden md:block'>
+                  {postedBy.userName}
+                </p>
+              </div>
+            </Link>
+            <Link href={`/detail/${_id}`}>
+              <p className='mt-2 font-normal '>{caption}</p>
+            </Link>
+          </div>
+        </div>
+       
+
+      </div>
+
+      <div className='w-80 flex gap-4 relative'>
+        <div
+          onMouseEnter={() => setIsHover(true)}
+          onMouseLeave={() => setIsHover(false)}
+          className='rounded-3xl'
+        >
+          <Link href={`/detail/${_id}`}>
+            <video
+              loop
+              ref={videoRef}
+              src={video.asset.url}
+              className='lg:w-[600px] h-[300px] md:h-[400px] lg:h-[528px] w-[200px] rounded-2xl cursor-pointer bg-gray-100'
+            ></video>
+          </Link>
+
+          {isHover && (
+            <div className='absolute bottom-6 cursor-pointer left-8 md:left-2 lg:left-0 flex gap-6 lg:justify-between w-[100px] md:w-[100px] lg:w-[350px] p-3'>
+              {playing ? (
+                <button onClick={onVideoPress}>
+                  <BsFillPauseFill className='text-black text-2xl lg:text-4xl' />
+                </button>
+              ) : (
+                <button onClick={onVideoPress}>
+                  <BsFillPlayFill className='text-black text-2xl lg:text-4xl' />
+                </button>
+            
+              )}
+              
+              {isVideoMuted ? (
+                <button onClick={() => setIsVideoMuted(false)}>
+                  <HiVolumeOff className='text-black text-2xl lg:text-4xl' />
+                </button>
+              ) : (
+                <button onClick={() => setIsVideoMuted(true)}>
+                  <HiVolumeUp className='text-black text-2xl lg:text-4xl' />
+                </button>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+      
+    </div>
+    <div className="w-[200px]  h-[200px] pt-40 pl-20">
+      <div className='flex font-sans text-lg text-center pt-4'>
+      <div className='bg-primary rounded-full p-2 md:p-4 text-[#F51997] '>
+          <MdFavorite className='text-lg md:text-2xl' />
+        </div>
+            
+      </div>
+      <div className='flex font-sans text-lg text-center  pt-4'>
+      <div className='bg-primary rounded-full p-2 md:p-4 text-[#F51997] '>
+          <FaComment className='text-lg md:text-2xl' />
+        </div>
+           
+      </div>
+      <div className='flex font-sans text-lg text-center pt-4'>
+      <div className='bg-primary rounded-full p-2 md:p-4 text-[#F51997] '>
+          <FaBell className='text-lg md:text-2xl' />
+        </div>
+        </div>
+          
+      <div className='flex font-sans text-lg text-center pt-4'>
+      <div className='bg-primary rounded-full p-2 md:p-4 text-[#F51997] '>
+          <FaUpload className='text-lg md:text-2xl' />
+        </div>
+           
+      </div>
+      </div>
+    </div>
+    <div className='flex flex-row'>
+    <div className='flex flex-col border-b-2 border-gray-200 pb-6'>
+      <div>
+        <div className='flex gap-3 p-2 cursor-pointer font-semibold rounded '>
+          <div className='md:w-16 md:h-16 w-10 h-10'>
+            <Link href={`/profile/${postedBy?._id}`}>
+              <>
+                <Image
+                  width={62}
+                  height={62}
+                  className=' rounded-full'
+                  src={postedBy?.image}
+                  alt='user-profile'
+                  layout='responsive'
+                />
+              </>
+            </Link>
+          </div>
+          <div>
+            <Link href={`/profile/${postedBy?._id}`}>
+              <div className='flex items-center gap-2'>
+                <p className='flex gap-2 items-center md:text-md font-bold text-primary'>
+                  {postedBy.userName}{' '}
+                  <GoVerified className='text-blue-400 text-md' />
+                </p>
+                <p className='capitalize font-medium text-xs text-gray-500 hidden md:block'>
+                  {postedBy.userName}
+                </p>
+              </div>
+            </Link>
+            <Link href={`/detail/${_id}`}>
+              <p className='mt-2 font-normal '>{caption}</p>
+            </Link>
+          </div>
+        </div>
+       
+
+      </div>
+
+      <div className='w-80 flex gap-4 relative'>
+        <div
+          onMouseEnter={() => setIsHover(true)}
+          onMouseLeave={() => setIsHover(false)}
+          className='rounded-3xl'
+        >
+          <Link href={`/detail/${_id}`}>
+            <video
+              loop
+              ref={videoRef}
+              src={video.asset.url}
+              className='lg:w-[600px] h-[300px] md:h-[400px] lg:h-[528px] w-[200px] rounded-2xl cursor-pointer bg-gray-100'
+            ></video>
+          </Link>
+
+          {isHover && (
+            <div className='absolute bottom-6 cursor-pointer left-8 md:left-2 lg:left-0 flex gap-6 lg:justify-between w-[100px] md:w-[100px] lg:w-[350px] p-3'>
+              {playing ? (
+                <button onClick={onVideoPress}>
+                  <BsFillPauseFill className='text-black text-2xl lg:text-4xl' />
+                </button>
+              ) : (
+                <button onClick={onVideoPress}>
+                  <BsFillPlayFill className='text-black text-2xl lg:text-4xl' />
+                </button>
+            
+              )}
+              
+              {isVideoMuted ? (
+                <button onClick={() => setIsVideoMuted(false)}>
+                  <HiVolumeOff className='text-black text-2xl lg:text-4xl' />
+                </button>
+              ) : (
+                <button onClick={() => setIsVideoMuted(true)}>
+                  <HiVolumeUp className='text-black text-2xl lg:text-4xl' />
+                </button>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+      
+    </div>
+    <div className="w-[200px]  h-[200px] pt-40 pl-20">
+      <div className='flex font-sans text-lg text-center pt-4'>
+      <div className='bg-primary rounded-full p-2 md:p-4 text-[#F51997] '>
+          <MdFavorite className='text-lg md:text-2xl' />
+        </div>
+            
+      </div>
+      <div className='flex font-sans text-lg text-center  pt-4'>
+      <div className='bg-primary rounded-full p-2 md:p-4 text-[#F51997] '>
+          <FaComment className='text-lg md:text-2xl' />
+        </div>
+           
+      </div>
+      <div className='flex font-sans text-lg text-center pt-4'>
+      <div className='bg-primary rounded-full p-2 md:p-4 text-[#F51997] '>
+          <FaBell className='text-lg md:text-2xl' />
+        </div>
+        </div>
+          
+      <div className='flex font-sans text-lg text-center pt-4'>
+      <div className='bg-primary rounded-full p-2 md:p-4 text-[#F51997] '>
+          <FaUpload className='text-lg md:text-2xl' />
+        </div>
+           
+      </div>
+      </div>
+    </div>
+   
+   
+    </>
   );
 };
 
