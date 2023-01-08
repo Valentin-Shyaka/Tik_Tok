86d5200053fc4eb2d92e09c49c3bd39c6cfabf22
Valentin-Shyaka <vava1lantern7@gmail.com>
2023-01-08 14:40:48 +0200

    next.config.js fie changes

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images:{
-    domains:['www.google.com','lh3.googleusercontent.com'],
+    domains:['www.google.com','lh3.googleusercontent.com','encrypted-tbn0.gstatic.com'],
   },
   
 }
